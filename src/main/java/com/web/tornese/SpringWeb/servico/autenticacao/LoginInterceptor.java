f863593ce0e6de659480bfc9f9be0ea7ed23bb32
Erick Panse <42036361+erick-panse@users.noreply.github.com>
2025-01-09 18:27:27 -0300

    Removendo comentários

--- src/main/java/com/web/tornese/SpringWeb/servico/autenticacao/LoginInterceptor.java
+++ src/main/java/com/web/tornese/SpringWeb/servico/autenticacao/LoginInterceptor.java
@@ -23,18 +23,4 @@ public class LoginInterceptor implements HandlerInterceptor {
     response.sendRedirect("/login");
     return false;
    }
-
-   // @Override
-   // public void postHandle(HttpServletRequest request, HttpServletResponse response, 
-   //    Object handler, ModelAndView modelAndView) throws Exception {
-      
-   //    System.out.println("Post Handle method is Calling");
-   // }
-   // @Override
-   // public void afterCompletion
-   //    (HttpServletRequest request, HttpServletResponse response, Object 
-   //    handler, Exception exception) throws Exception {
-      
-   //    System.out.println("Request and Response is completed");
-   // }
 }
